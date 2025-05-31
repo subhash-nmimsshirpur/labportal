1a705ed41ea763a64e48a4eeb4d7883572d20cc7
Subhash Bhalerao <subhash.nmims.shirpur@gmail.com>
2025-05-31 13:39:54 +0530

    Made static file settings

--- labportal/settings.py
+++ labportal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,6 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# The absolute path to the directory where static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Base URL to serve media files
+MEDIA_URL = 'media/'
+
+# The absolute path to the directory where media files will be collected
+MEDIA_ROOT = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
